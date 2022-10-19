e6d4ac6fa0a35dc6ab3c77744d0df90d138f16cb
paul <paulghetea@outlook.es>
2022-10-19 14:47:18 +0200

    servicio feign intergado

--- springboot-servicio-item/src/main/java/com/formacion/springboot/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacion/springboot/item/SpringbootServicioItemApplication.java
@@ -2,7 +2,10 @@ package com.formacion.springboot.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//Activar Feign
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
