cae7f7a33f135d0eb5bef3c75e10033d62729dd0
paul <paulghetea@outlook.es>
2022-10-19 13:27:23 +0200

    cliente rest aplicado

---
+++ springboot-servicio-item/src/main/java/com/formacion/springboot/item/entities/Producto.java
@@ -0,0 +1,17 @@
+package com.formacion.springboot.item.entities;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Producto {
+	
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private Date fechaDeCreacion;
+
+}
---
+++ springboot-servicio-item/src/test/java/com/formacion/springboot/item/SpringbootServicioItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.springboot.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
