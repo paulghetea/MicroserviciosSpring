034742f5e15d3bfbaa35f6264edea2e54ed6e1cf
paul <paulghetea@outlook.es>
2022-10-26 10:10:49 +0200

    perfiles en config

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,18 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,29 +24,53 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
-	
+
+	private Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
-	
+	@Value("${configuracion.texto}")
+	private String texto;
+
+	@GetMapping("/obtenerConfig")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		logger.info(texto);
+		// Creamos un json
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {// si existen perfiles y es el de development
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
 	@GetMapping("/listar")
-	public List<Item> listar(@RequestParam(name="nombre", required = false) String nombre, @RequestHeader(name="token-request", required = false) String token) /*inyectamos desde el request*/{
+	public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
+			@RequestHeader(name = "token-request", required = false) String token) /* inyectamos desde el request */ {
 		System.out.println(nombre);
 		System.out.println(token);
 		return itemService.findAll();
 	}
-	
-	//En caso de fallo, ejecutamos otro método.
+
+	// En caso de fallo, ejecutamos otro método.
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
-	//metodo alternativo en caso de error.
-	public Item metodoAlternativo(Long id, Integer cantidad){
+
+	// metodo alternativo en caso de error.
+	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
