4ff89d7501a8afe478887aecd6251a9af010ebed
paul <paulghetea@outlook.es>
2022-11-02 10:48:57 +0100

    exponer id

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email ) VALUES('paulgh00', '800', 1, 'Paul', 'Ghetea', 'paulghetea@outlook.es');
 INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email ) VALUES('maria', '123', 1, 'Maria', 'Menchero', 'mariamenchero@outlook.es');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_to_roles (usuario_id, role_id) VALUES(1,1);
+INSERT INTO usuarios_to_roles  (usuario_id, role_id) VALUES(2,1);
+INSERT INTO usuarios_to_roles  (usuario_id, role_id) VALUES(2,2);
